6ec18f99e9edcfe3b62f9e68345889e1a9a8609e
vanessamachado93 <machado.vanessa32@gmail.com>
2023-02-20 09:43:06 -0300

    Add state locking

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "4.0"
     }
   }
+  backend "s3" {
+    bucket = "empresax-terraform-state"
+    key = "dev/banck/terrafomr.tfstate"
+    region = "us-east-1"
+    dynamodb_endpoint = "terraform-lock-state"
+  }
 }
